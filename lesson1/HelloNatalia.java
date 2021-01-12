ececc3679f2be29a37a2467c2c35b697c6e313f3
unknown <Natalya@KROLIKOVR.itransition.corp>
2021-01-12 17:14:27 +0300

    Add HelloNatalia class

---
+++ lesson1/HelloNatalia.java
@@ -0,0 +1,12 @@
+public class HelloNatalia {
+	
+	public static void main (String ... args) throws Exception {
+		
+		while(true) {
+		System.out.println("Hello Natalia!!!");
+		Thread.sleep(1000);
+		}
+		
+	}
+	
+}
