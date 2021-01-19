11f29ed311f695ce986611ac7448808e5e6aea92
Natalia <n.krolikova@gmail.com>
2021-01-19 17:01:15 +0300

    Задание4

---
+++ lesson2/src/ReversNumber.java
@@ -0,0 +1,15 @@
+public class ReversNumber {
+    public static void main(String[] args) {
+
+        int n = 1634;
+        int a = n%1000;
+        int b = a%100;
+        int n4 = b%10;
+        int n3 = (b-n4)/10;
+        int n2 = (a-b)/100;
+        int n1 = (n-a)/1000;
+
+        System.out.println(n);
+        System.out.println(n4+""+n3+""+n2+""+n1);
+    }
+}
