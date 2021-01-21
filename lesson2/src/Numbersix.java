db908b444744b14a65503802709e4d6d6a3ad711
Natalia <n.krolikova@gmail.com>
2021-01-21 16:51:00 +0300

    lesson4

---
+++ lesson2/src/Numbersix.java
@@ -0,0 +1,25 @@
+public class Numbersix {
+
+    public static void main(String[] args) {
+
+        int n = 333;
+        int a = n%100;
+        int n3 = a%10;
+        int n2 = (a-n3)/10;
+        int n1 = (n-a)/100;
+
+        if (n<1000) {
+            System.out.println("Число n является трехзначным");
+
+        }
+            System.out.println(n3 == 7);
+
+        if (n%2==0) {
+            System.out.println("Число " + n + " является четным");
+        } else {
+            System.out.println("Число " + n + " не является четным");
+        }
+
+
+    }
+}
---
+++ lesson2/src/True.java
@@ -0,0 +1,23 @@
+public class True {
+
+    public static void main(String[] args) {
+
+        int n = 4567;
+        int a = n%1000;
+        int b = a%100;
+        int n4 = b%10;
+        int n3 = (b-n4)/10;
+        int n2 = (a-b)/100;
+        int n1 = (n-a)/1000;
+
+        System.out.println(n);
+        System.out.println(n1 != n2);
+        System.out.println(n1 != n3);
+        System.out.println(n1 != n4);
+        System.out.println(n2 != n3);
+        System.out.println(n2 != n4);
+        System.out.println(n3 != n4);
+
+
+    }
+}
