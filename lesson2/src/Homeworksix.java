abca899971fc6210dd7ea99f8d445a0db71a2a14
Natalia <n.krolikova@gmail.com>
2021-01-22 15:47:23 +0300

    Задание4

---
+++ lesson2/src/Homeworksix.java
@@ -0,0 +1,15 @@
+//Даны 2 числа. Вывести большее из них
+
+public class Homeworksix {
+
+    public static void main(String[] args) {
+
+        int a = 555;
+        int b = 1313;
+
+        if (a>b) {
+            System.out.println(a);
+        } else
+            System.out.println(b);
+    }
+}
